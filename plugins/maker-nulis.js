2efc8802f975cd93586188d6655c6d51647c9902
habibadha <76233615+habibadha@users.noreply.github.com>
2022-07-21 19:57:38 +0700

    Update maker-nulis.js

--- plugins/maker-nulis.js
+++ plugins/maker-nulis.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, args }) => {
   let tgl = d.toLocaleDateString('id-Id')
   let hari = d.toLocaleDateString('id-Id', { weekday: 'long' })
   let teks = args.join` `
-  conn.reply(m.chat, util.format({fontPath, inputPath, outputPath, tgl, hari, teks}), m)
+  //conn.reply(m.chat, util.format({fontPath, inputPath, outputPath, tgl, hari, teks}), m)
   let bufs = []
   const [_spawnprocess, ..._spawnargs] = [...(global.support.gm ? ['gm'] : global.support.magick ? ['magick'] : []),
     'convert',
