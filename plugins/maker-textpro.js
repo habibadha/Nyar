b2ad0ae2e5403c8e3e416339b051441bbe262a9a
habibadha <76233615+habibadha@users.noreply.github.com>
2022-07-22 14:21:44 +0700

    Update maker-textpro.js

--- plugins/maker-textpro.js
+++ plugins/maker-textpro.js
@@ -1,8 +1,7 @@
 let handler = async (m, { conn, command, text }) => {
 if (!text) throw 'Masukkan Text... '
 let nyenye = `https://api.lolhuman.xyz/api/textprome/${command}?apikey=ff6e31baacb07e58c260d893&text=${text}`
-    conn.sendFile(m.chat, nyenye, null, 'Nih', m)
-} 
+conn.sendFile(m.chat, Buffer.concat(bufs), 'hasil.jpg', 'nih', m) 
 }
 handler.help = ['blackpink', 'neon', 'greenneon', 'futureneon', 'sandwriting', 'sandsummer', 'sandengraved', 'metaldark', 'neonlight', 'holographic', 'text1917', 'minion', 'deluxesilver', 'newyearcard', 'bloodfrosted', 'hallowen', 'fireworksparkle', 'natureleaves', 'bokeh', 'toxic', 'strawberry', 'box3d', 'roadwarning', 'breakwall', 'icecold', 'luxury', 'cloud', 'summersand', 'horrorblood', 'thunder', 'magma', 'impressiveglitch', 'harrypotter', 'foggywindow', 'watercolor', 'wonderfulgrafitti'] 
 handler.tags = ['maker']
